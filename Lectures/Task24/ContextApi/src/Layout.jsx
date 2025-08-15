46861f3960acbf357b96b86f8adcba83ec943d87
Amaresh Sahoo <amareshsahoo950@gmail.com>
2025-08-15 13:16:34 +0530

    modification in page state.

--- Lectures/Task24/ContextApi/src/Layout.jsx
+++ Lectures/Task24/ContextApi/src/Layout.jsx
@@ -1,17 +1,24 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { Header, Cart } from './Index'
 import { CartProvider } from "./Context/context";
 import "./Style/Layout.css";
 
 function Layout() {
+
+  const location = useLocation();
+
   const [cart, setCart] = useState(() =>
     JSON.parse(localStorage.getItem('cart')) || []);
   const [isHomePage, setIsHomePage] = useState(() =>
-    JSON.parse(localStorage.getItem('homeState')) ?? true);
+    JSON.parse(localStorage.getItem('homeState')) || true);
 
   useEffect(() => {
     localStorage.setItem("cart", JSON.stringify(cart));
+
+    const currentPageState = location.pathname === '/';
+    setIsHomePage(currentPageState);
+
     localStorage.setItem("homeState", JSON.stringify(isHomePage));
   }, [cart, isHomePage])
 
